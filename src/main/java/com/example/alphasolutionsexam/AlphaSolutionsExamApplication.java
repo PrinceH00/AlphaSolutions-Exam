b9f0dc05e6a38164fcd0ba4937cbba803e7b0556
hknocal <hknocal@outlook.com>
2023-04-25 15:48:56 +0200

    added dependencies

---
+++ src/main/java/com/example/alphasolutionsexam/AlphaSolutionsExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.alphasolutionsexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaSolutionsExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaSolutionsExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/alphasolutionsexam/AlphaSolutionsExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alphasolutionsexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaSolutionsExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
