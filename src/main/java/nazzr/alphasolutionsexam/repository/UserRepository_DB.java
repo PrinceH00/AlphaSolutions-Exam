1e42f80da11df674ed88e52bcbcd73d3714b1b70
Zana0046@stud.kea.dk <90389855+Dannicolak@users.noreply.github.com>
2023-05-02 16:12:39 +0200

    createUser done

--- src/main/java/nazzr/alphasolutionsexam/repository/UserRepository_DB.java
+++ src/main/java/nazzr/alphasolutionsexam/repository/UserRepository_DB.java
@@ -4,6 +4,11 @@ import nazzr.alphasolutionsexam.model.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 @Repository
 public class UserRepository_DB implements IUserRepository_DB {
 
@@ -16,6 +21,16 @@ public class UserRepository_DB implements IUserRepository_DB {
 
     @Override
     public void createUser(User user) {
+        try (Connection con = DriverManager.getConnection(url, username, pwd)) {
+            String SQL = "INSERT INTO User(firstname, lastname, email) VALUES (?, ?, ?)";
+            PreparedStatement preparedStatement = con.prepareStatement(SQL);
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setString(2, user.getLastName());
+            preparedStatement.setString(3, user.getEmail());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
     }
 
     @Override
