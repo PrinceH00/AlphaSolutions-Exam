6971d090c2fdee5c0bf8b0c6f1b59127848eba69
PrinceH00 <hamz0230@stud.kea.dk>
2023-05-02 13:33:06 +0200

    Model setup with get/set and constructor

--- src/main/java/nazzr/alphasolutionsexam/model/Project.java
+++ src/main/java/nazzr/alphasolutionsexam/model/Project.java
@@ -11,8 +11,7 @@ public class Project {
     private Date finalDate;
     private boolean isDone;
 
-    public Project() {
-    }
+    public Project() {}
 
     public Project(int projectID, String title, String discription, Date startDate, Date deadlineDate, Date finalDate, boolean isDone) {
         this.projectID = projectID;
@@ -24,66 +23,21 @@ public class Project {
         this.isDone = isDone;
     }
 
-    public int getProjectID() {
-        return projectID;
-    }
+    //----------------------------------------------------Getter-----------------------------------------------------\\
+    public int getProjectID() { return projectID; }
+    public String getTitle() { return title; }
+    public String getDiscription() { return discription; }
+    public Date getStartDate() { return startDate; }
+    public Date getDeadlineDate() { return deadlineDate; }
+    public Date getFinalDate() { return finalDate; }
+    public boolean isDone() { return isDone; }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDiscription() {
-        return discription;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public Date getDeadlineDate() {
-        return deadlineDate;
-    }
-
-    public Date getFinalDate() {
-        return finalDate;
-    }
-
-    public boolean isDone() {
-        return isDone;
-    }
-
-    public Project setProjectID(int projectID) {
-        this.projectID = projectID;
-        return this;
-    }
-
-    public Project setTitle(String title) {
-        this.title = title;
-        return this;
-    }
-
-    public Project setDiscription(String discription) {
-        this.discription = discription;
-        return this;
-    }
-
-    public Project setStartDate(Date startDate) {
-        this.startDate = startDate;
-        return this;
-    }
-
-    public Project setDeadlineDate(Date deadlineDate) {
-        this.deadlineDate = deadlineDate;
-        return this;
-    }
-
-    public Project setFinalDate(Date finalDate) {
-        this.finalDate = finalDate;
-        return this;
-    }
-
-    public Project setDone(boolean done) {
-        isDone = done;
-        return this;
-    }
+    //----------------------------------------------------Setter-----------------------------------------------------\\
+    public void setProjectID(int projectID) { this.projectID = projectID; }
+    public void setTitle(String title) { this.title = title; }
+    public void setDiscription(String discription) { this.discription = discription; }
+    public void setStartDate(Date startDate) { this.startDate = startDate; }
+    public void setDeadlineDate(Date deadlineDate) { this.deadlineDate = deadlineDate; }
+    public void setFinalDate(Date finalDate) { this.finalDate = finalDate; }
+    public void setDone(boolean done) { isDone = done; }
 }
