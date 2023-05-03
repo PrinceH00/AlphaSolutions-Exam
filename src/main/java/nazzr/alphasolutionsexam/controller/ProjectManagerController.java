66efc1ed36e43ad88ed2527dc19a00fc9dbe7a1d
PrinceH00 <hamz0230@stud.kea.dk>
2023-05-03 13:00:50 +0200

    conected service and added is loggedIn

--- src/main/java/nazzr/alphasolutionsexam/controller/ProjectManagerController.java
+++ src/main/java/nazzr/alphasolutionsexam/controller/ProjectManagerController.java
@@ -1,7 +1,22 @@
 package nazzr.alphasolutionsexam.controller;
 
+import jakarta.servlet.http.HttpSession;
+import nazzr.alphasolutionsexam.service.LoginService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ProjectManagerController {
+
+    private final LoginService loginService;
+
+    public ProjectManagerController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    private boolean isLoggedIn(HttpSession session) {
+        return session.getAttribute("user") != null;
+    }
+
+
+
 }
