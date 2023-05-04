1241b2455e6136950480fa652348841e2b30ee4c
hknocal <hknocal@outlook.com>
2023-05-04 14:28:03 +0200

    createProject method

--- src/main/java/nazzr/alphasolutionsexam/model/Project.java
+++ src/main/java/nazzr/alphasolutionsexam/model/Project.java
@@ -10,10 +10,11 @@ public class Project {
     private Date deadlineDate;
     private Date finalDate;
     private boolean isDone;
+    private int userID;
 
     public Project() {}
 
-    public Project(int projectID, String title, String description, Date startDate, Date deadlineDate, Date finalDate, boolean isDone) {
+    public Project(int projectID, String title, String description, Date startDate, Date deadlineDate, Date finalDate, boolean isDone, int userID) {
         this.projectID = projectID;
         this.title = title;
         this.description = description;
@@ -21,6 +22,7 @@ public class Project {
         this.deadlineDate = deadlineDate;
         this.finalDate = finalDate;
         this.isDone = isDone;
+        this.userID = userID;
     }
 
     //----------------------------------------------------Getter-----------------------------------------------------\\
@@ -31,6 +33,9 @@ public class Project {
     public Date getDeadlineDate() { return deadlineDate; }
     public Date getFinalDate() { return finalDate; }
     public boolean isDone() { return isDone; }
+    public int getUserID() {
+        return userID;
+    }
 
     //----------------------------------------------------Setter-----------------------------------------------------\\
     public void setProjectID(int projectID) { this.projectID = projectID; }
@@ -40,4 +45,7 @@ public class Project {
     public void setDeadlineDate(Date deadlineDate) { this.deadlineDate = deadlineDate; }
     public void setFinalDate(Date finalDate) { this.finalDate = finalDate; }
     public void setDone(boolean done) { isDone = done; }
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
 }
--- src/main/java/nazzr/alphasolutionsexam/repository/ProjectManagerRepository_DB.java
+++ src/main/java/nazzr/alphasolutionsexam/repository/ProjectManagerRepository_DB.java
@@ -1,4 +1,35 @@
 package nazzr.alphasolutionsexam.repository;
 
-public class ProjectManagerRepository_DB {
+import nazzr.alphasolutionsexam.model.Project;
+import nazzr.alphasolutionsexam.repository.util.DB_Connector;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+
+@Repository
+public class ProjectManagerRepository_DB implements IProjectManagerRepository_DB {
+    String SQL = null;
+    Connection connection = DB_Connector.getConnection();
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+
+    public Project createProject(Project project) {
+        try {
+            SQL = "INSERT INTO Project (title, description, startDate, deadlineDate, finalDate, isDone, user_id) VALUES(?,?,?,?,?,?,?))";
+            PreparedStatement ps = connection.prepareStatement(SQL);
+
+            preparedStatement.setString(1, project.getTitle());
+            preparedStatement.setString(2, project.getDescription());
+            preparedStatement.setDate(3, (Date) project.getStartDate());
+            preparedStatement.setDate(4, (Date) project.getDeadlineDate());
+            preparedStatement.setDate(5, (Date) project.getFinalDate());
+            preparedStatement.setBoolean(6, project.isDone());
+            ps.executeUpdate();
+
+            return project;
+
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+    }
 }
