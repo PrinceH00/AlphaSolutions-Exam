ad128f0244f885a85ac25653aa43eca211d459d1
PrinceH00 <hamz0230@stud.kea.dk>
2023-05-05 11:36:23 +0200

    addedgetAllProjects in IProjectManagerRepository_DB

--- src/main/java/nazzr/alphasolutionsexam/repository/IProjectManagerRepository_DB.java
+++ src/main/java/nazzr/alphasolutionsexam/repository/IProjectManagerRepository_DB.java
@@ -1,7 +1,14 @@
 package nazzr.alphasolutionsexam.repository;
 
+import nazzr.alphasolutionsexam.DTO.ViewProjectDTO;
 import nazzr.alphasolutionsexam.model.Project;
+import nazzr.alphasolutionsexam.model.User;
+
+import java.util.List;
 
 public interface IProjectManagerRepository_DB {
+
     Project createProject(Project project);
+
+    public List<ViewProjectDTO> getAllProjects(User user);
 }
