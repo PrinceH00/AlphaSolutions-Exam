ff19efd920f2f0c00dabbd3655a55e36aadbd59d
hknocal <hknocal@outlook.com>
2023-05-06 20:25:07 +0200

    removed redundant boolean

--- src/main/java/nazzr/alphasolutionsexam/model/SubTask.java
+++ src/main/java/nazzr/alphasolutionsexam/model/SubTask.java
@@ -6,33 +6,55 @@ public class SubTask {
     private String description;
     private int estimated_time;
     private int final_time;
-    private boolean isDone;
 
-    public SubTask() {}
+    public SubTask() {
+    }
 
-    public SubTask(int subTaskID, String title, String description, int estimated_time, int final_time, boolean isDone) {
+    public SubTask(int subTaskID, String title, String description, int estimated_time, int final_time) {
         this.subTaskID = subTaskID;
         this.title = title;
         this.description = description;
         this.estimated_time = estimated_time;
         this.final_time = final_time;
-        this.isDone = isDone;
     }
 
-    //----------------------------------------------------Getter-----------------------------------------------------\\-\\
-    public int getSubTaskID() { return subTaskID; }
-    public String getTitle() { return title; }
-    public String getDescription() { return description; }
-    public int getEstimated_time() { return estimated_time; }
-    public int getFinal_time() { return final_time; }
-    public boolean isDone() { return isDone; }
+    public int getSubTaskID() {
+        return subTaskID;
+    }
 
-    //----------------------------------------------------Setter-----------------------------------------------------\\
-    public void setSubTaskID(int subTaskID) { this.subTaskID = subTaskID; }
-    public void setTitle(String title) { this.title = title; }
-    public void setDescription(String description) { this.description = description; }
-    public void setEstimated_time(int estimated_time) { this.estimated_time = estimated_time; }
-    public void setFinal_time(int final_time) { this.final_time = final_time; }
-    public void setDone(boolean done) { isDone = done; }
+    public void setSubTaskID(int subTaskID) {
+        this.subTaskID = subTaskID;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getEstimated_time() {
+        return estimated_time;
+    }
+
+    public void setEstimated_time(int estimated_time) {
+        this.estimated_time = estimated_time;
+    }
+
+    public int getFinal_time() {
+        return final_time;
+    }
+
+    public void setFinal_time(int final_time) {
+        this.final_time = final_time;
+    }
 }
--- src/main/java/nazzr/alphasolutionsexam/model/Task.java
+++ src/main/java/nazzr/alphasolutionsexam/model/Task.java
@@ -4,26 +4,36 @@ public class Task {
     private int TaskID;
     private String title;
     private String description;
-    private boolean isDone;
 
     public Task() {}
 
-    public Task(int taskID, String title, String description, boolean isDone) {
+    public Task(int taskID, String title, String description) {
         TaskID = taskID;
         this.title = title;
         this.description = description;
-        this.isDone = isDone;
     }
 
-    //----------------------------------------------------Getter-----------------------------------------------------\\
-    public int getTaskID() { return TaskID; }
-    public String getTitle() { return title; }
-    public String getDescription() { return description; }
-    public boolean isDone() { return isDone; }
+    public int getTaskID() {
+        return TaskID;
+    }
 
-    //----------------------------------------------------Setter------------------------------------------------------\\
-    public void setTaskID(int taskID) { TaskID = taskID; }
-    public void setTitle(String title) { this.title = title; }
-    public void setDescription(String description) { this.description = description; }
-    public void setDone(boolean done) { isDone = done; }
+    public void setTaskID(int taskID) {
+        TaskID = taskID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
