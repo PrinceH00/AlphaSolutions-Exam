fce4a0bdf39f03059e6af12f835ad89b13678ee9
hknocal <hknocal@outlook.com>
2023-05-11 17:45:32 +0200

    added missing update on cascade

--- src/main/resources/sql/ProjectManagerDB.sql
+++ src/main/resources/sql/ProjectManagerDB.sql
@@ -18,10 +18,10 @@ CREATE TABLE Employee
     firstname   VARCHAR(30)  NOT NULL,
     lastname    VARCHAR(30)  NOT NULL,
     email       VARCHAR(100) NOT NULL UNIQUE,
-    job        VARCHAR(30)  NOT NULL,
+    job         VARCHAR(30)  NOT NULL,
     user_id     INT(10) NOT NULL,
     PRIMARY KEY (employee_id),
-    FOREIGN KEY (user_id) REFERENCES User (user_id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES User (user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Project
@@ -34,7 +34,7 @@ CREATE TABLE Project
     finalDate    DATE,
     user_id      INT(10) NOT NULL,
     PRIMARY KEY (project_id),
-    FOREIGN KEY (user_id) REFERENCES User (user_id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES User (user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Task
@@ -44,7 +44,7 @@ CREATE TABLE Task
     description VARCHAR(255),
     project_id  INT(10) NOT NULL,
     PRIMARY KEY (task_id),
-    FOREIGN KEY (project_id) REFERENCES Project (project_id) ON DELETE CASCADE
+    FOREIGN KEY (project_id) REFERENCES Project (project_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Subtask
@@ -56,7 +56,7 @@ CREATE TABLE Subtask
     final_time     INT(10),
     task_id        INT(10) NOT NULL,
     PRIMARY KEY (subtask_id),
-    FOREIGN KEY (task_id) REFERENCES Task (task_id) ON DELETE CASCADE
+    FOREIGN KEY (task_id) REFERENCES Task (task_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE EmployeeSubtask
@@ -65,6 +65,6 @@ CREATE TABLE EmployeeSubtask
     employee_id        INT(10) NOT NULL,
     subtask_id         INT(10) NOT NULL,
     PRIMARY KEY (employeesubtask_id),
-    FOREIGN KEY (employee_id) REFERENCES Employee (employee_id) ON DELETE CASCADE,
-    FOREIGN KEY (subtask_id) REFERENCES Subtask (subtask_id) ON DELETE CASCADE
+    FOREIGN KEY (employee_id) REFERENCES Employee (employee_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (subtask_id) REFERENCES Subtask (subtask_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
