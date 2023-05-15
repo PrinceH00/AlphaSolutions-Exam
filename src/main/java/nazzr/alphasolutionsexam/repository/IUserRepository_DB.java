d74de382a64c7f115b02cf8b1318ebc67cbf9043
PrinceH00 <hamz0230@stud.kea.dk>
2023-05-15 02:14:22 +0200

    Refactored IUserRepository_DB

--- src/main/java/nazzr/alphasolutionsexam/repository/IUserRepository_DB.java
+++ src/main/java/nazzr/alphasolutionsexam/repository/IUserRepository_DB.java
@@ -3,6 +3,8 @@ package nazzr.alphasolutionsexam.repository;
 import nazzr.alphasolutionsexam.model.User;
 
 public interface IUserRepository_DB {
+
+    //---------------------------------------------------USER---------------------------------------------------------\\
     User createUser(User user);
 
     User getUser(String email, String password);
