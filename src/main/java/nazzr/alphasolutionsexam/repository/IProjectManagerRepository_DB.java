2c2f5444b9d5735fb40756633d9ad89712b40d94
PrinceH00 <hamz0230@stud.kea.dk>
2023-05-15 02:10:26 +0200

    Refactored IProjectManagerRepository_DB

--- src/main/java/nazzr/alphasolutionsexam/repository/IProjectManagerRepository_DB.java
+++ src/main/java/nazzr/alphasolutionsexam/repository/IProjectManagerRepository_DB.java
@@ -1,25 +1,40 @@
 package nazzr.alphasolutionsexam.repository;
 
 import nazzr.alphasolutionsexam.model.*;
-
 import java.util.List;
 
 public interface IProjectManagerRepository_DB {
 
+    //--------------------------------------------------PROJECT-------------------------------------------------------\\
     Project createProject(Project project);
+
     void deleteProject(int projectID);
+
     Project getProjectByID(int project_id);
+
     List<Project> getAllProjects(User user);
-    Task createTask(Task task, int project_id);
-    Task getTaskByID(int task_id);
-    List<Task> getAllTasks(int project_id);
-    void deleteTask(int taskID);
-    Subtask createSubtask(Subtask subtask, int taskID);
-    List<Subtask> getSubtasks(int taskID);
-    void deleteSubtask(int taskID);
+
+    //--------------------------------------------------EMPLOYEE------------------------------------------------------\\
     Employee createEmployee(Employee employee);
 
     List<Employee> getEmployees(User user);
 
     void deleteEmployee(int employeeID);
+
+    //---------------------------------------------------TASK---------------------------------------------------------\\
+    Task createTask(Task task, int project_id);
+
+    Task getTaskByID(int task_id);
+
+    List<Task> getAllTasks(int project_id);
+
+    void deleteTask(int taskID);
+
+    //--------------------------------------------------SUBTASK-------------------------------------------------------\\
+    Subtask createSubtask(Subtask subtask, int taskID);
+
+    List<Subtask> getSubtasks(int taskID);
+
+    void deleteSubtask(int taskID);
+
 }
